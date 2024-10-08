e84fd732cb6aa45d4f2be01b50614fc293c74ee7
aditya-pushkar <adityapushkar850@gmail.com>
2024-10-09 04:08:34 +0530

    / and /chat route added

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,24 +1,21 @@
+import { Routes, Route } from 'react-router-dom'
+
+import Documents from './pages/Documents'
+import Chat from './pages/Chat'
+
 import TitleBar from './components/title-bar/TitleBar'
 
 function App(): JSX.Element {
   // const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
   return (
-    <>
+    <div className="w-screen">
       <TitleBar />
-      <h1 className="text-xl">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis repellendus aliquid
-        vero modi illum, nobis tenetur placeat dolore corporis omnis optio officiis quis aperiam,
-        velit nulla reprehenderit temporibus possimus dolorum.
-      </h1>
-      <button className="btn btn-primary">Click me</button>
-      <button className="btn btn-secondary rounded-full btn-sm">Click me</button>
-      <button className="btn btn-accent">Click me</button>
-      <button className="btn">Click me</button>
-      <button className="btn btn-neutral">Click me</button>
-      <button className="btn btn-success">Click me</button>
-      <button className="btn btn-warning">Click me</button>
-    </>
+      <Routes>
+        <Route path="/" element={<Documents />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </div>
   )
 }
 
