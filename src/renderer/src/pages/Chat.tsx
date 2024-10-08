0d0483052d254618947a0dc7891ac8c12e1fbd06
aditya-pushkar <adityapushkar850@gmail.com>
2024-10-09 04:07:42 +0530

    /documents and /chat page added

---
+++ src/renderer/src/pages/Chat.tsx
@@ -0,0 +1,5 @@
+const Chat = (): JSX.Element => {
+  return <div>Chats</div>
+}
+
+export default Chat
---
+++ src/renderer/src/pages/Documents.tsx
@@ -0,0 +1,11 @@
+import { Link } from "react-router-dom"
+
+const Documents = (): JSX.Element => {
+  return (
+    <div>
+        <Link to="/chat" className="btn btn-secondary">/chat</Link>
+    </div>
+  )
+}
+
+export default Documents
