23420bb395abe80ea1aee109d82c804080071e30
aditya-pushkar <adityapushkar850@gmail.com>
2024-10-17 20:10:26 +0530

    get downloded models

--- src/renderer/src/components/title-bar/SelectOllamaModel.tsx
+++ src/renderer/src/components/title-bar/SelectOllamaModel.tsx
@@ -1,14 +1,27 @@
+import { useEffect, useState } from 'react'
+
 const SelectOllamaModel = (): JSX.Element => {
+  const [models, setModels] = useState<object[] | null>(null)
+  useEffect(() => {
+    fetchModels()
+  }, [])
+
+  const fetchModels = async (): Promise<void> => {
+    const res = await fetch('http://localhost:11434/api/tags')
+    const modelsObj = await res.json()
+    if (modelsObj) {
+      setModels(modelsObj.models)
+    }
+  }
   return (
     <>
       <select className="select select-bordered  min-w-64 max-w-xs select-sm text-center">
         <option disabled selected className="">
           Select your model
         </option>
-        <option>llama3.2:1b</option>
-        <option>llama3.2:latest </option>
-        <option>gemma2:2b</option>
-        <option>llama3.2:3b</option>
+        {models != null
+          ? models.map((model, index) => <option key={index}>{model.name}</option>)
+          : ''}
       </select>
     </>
   )
